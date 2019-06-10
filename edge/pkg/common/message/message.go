e4498b7b9f5a6b943b22c207a34a7cba5bdd9eff
maurya-anuj <46441297+maurya-anuj@users.noreply.github.com>
2019-06-10 16:08:13 +0530

    Update message.go

--- edge/pkg/common/message/message.go
+++ edge/pkg/common/message/message.go
@@ -7,12 +7,13 @@ import (
 //constant defining node connection types
 const (
 	ResourceTypeNodeConnection = "node/connection"
-	OperationNodeConnection    = "publish"
+	
+	OperationNodeConnection = "publish"
 	SourceNodeConnection       = "edgehub"
 )
 
 //BuildMsg returns message object with router and content details
-func BuildMsg(group, parentID, sourceName, resource, operation string, content interface{}) *model.Message {
+func BuildMsg(group, parentId, sourceName, resource, operation string, content interface{}) *model.Message {
 	msg := model.NewMessage(parentID).BuildRouter(sourceName, group, resource, operation).FillBody(content)
 	return msg
 }
